f7761054f1ca9918a95e7bee952f6509480597b1
kishorekolluru <devkolluru@gmail.com>
2016-08-19 17:04:18 -0400

    initial files checkin

---
+++ src/Jelper.java
@@ -0,0 +1,339 @@
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Jelper {
+
+	protected static StringBuilder gendCode = new StringBuilder();
+	protected static StringBuilder gendJdbcCode = new StringBuilder();
+	// Match Levels
+	protected static final String PERFECT_MATCH = "perfect";
+	protected static final String IGNORE_CASE_IND_WORDS = "ignore_case_for_individual_words";
+	// Punctuations
+	protected static final String PERIOD = ".";
+	protected static final String OPEN_PARAN = "(";
+	protected static final String CLOSED_PARAN = ")";
+	protected static final String PAIR_PARAN = "()";
+	protected static final String SEMI_COLON = ";";
+	protected static final String NEW_LINE = "\n";
+	protected static final char DOUBLE_QUOTE='"';
+	// SET and GET
+	protected static final String SET = "SET";
+	protected static final String GET = "GET";
+	protected static final String TRY_MATCH_SHORTENED = "match_shortened";
+	protected static final String TRY_MATCH_SHORTENED_SHUFFLED = "match_shortened_shuffled";
+	protected static final String MATCH_REMAINING_TYPE = "remaining";
+
+	protected static final Map<Class, Class> compatTypes = new HashMap<Class, Class>();
+	static{
+		compatTypes.put(int.class, BigDecimal.class);
+		compatTypes.put(BigDecimal.class, int.class);
+	}
+	private static int countGend=0;
+	private static int countWantd=0;
+
+	private static Map<Method, Method> codeMap = new HashMap<Method, Method>();
+
+	/**This method generates JDBC Dao code by matching the columns with the setter methods
+	 * @param set - The Class name for which to set the values to
+	 * @param columns- List of Column names
+	 * @return
+	 * @throws ClassNotFoundException
+	 */
+	public static String jelperJdbcCodeGen(String set, List<String> columns) throws ClassNotFoundException{
+		Class setClass = null;
+
+		//cleanColumns= Util.cleanseColumns(columns);
+		setClass = ClassLoader.getSystemClassLoader().loadClass(set);
+		List<Method> setClassMethods = JelperUtil.getClassMethods(setClass, SET);
+		countWantd=setClassMethods.size();
+		if(setClassMethods.size()!=0 && columns.size()!=0){
+			gendJdbcCode.append(JelperUtil.getResSetInstCode());
+			gendJdbcCode.append(JelperUtil.genInstantiationCode(setClass));
+			matchGetterWithColumn(setClassMethods, columns);
+		}
+		System.out.println(countGend +" setters generated of " + countWantd);
+		return gendJdbcCode.toString();
+	}
+	private static void matchGetterWithColumn(List<Method> setClassMethods,
+			List<String> columns) {
+		String setName=null;
+		String column=null;
+		boolean flag=false;
+		for( int i=0 ; i<setClassMethods.size(); ){
+			setName= setClassMethods.get(i).getName().substring(3);
+			for( int j=0 ; j<columns.size();){
+				column= columns.get(j);
+				if(JelperUtil.isColumnMatching(setName,column)){
+					genJdbcCode(setClassMethods.get(i), column);
+
+					countGend++;
+					setClassMethods.remove(i);
+					columns.remove(j);
+					flag=true;
+					i=j=0;
+				}
+				else{
+					j++;
+				}
+			}
+			if(!flag){
+				i++;
+			}
+		}
+
+	}
+	/**This method generates setter code on the class instance that is being "set". The method
+	 * accesses all the setter methods of the set class and tries to match the getters with the setters
+	 * Eg. a setter setDrvrDet() of type Class1 and getter getDriverDetails() Class2 will result in the code,
+	 * 		 class1.setDrvrDet(class2.getDriverDetails());
+	 * Also the code generated follows dictionary order on the setter methods.
+	 *
+	 * @param get- The Class name on which getters should be called
+	 * @param set- The Class name on which setters should be called
+	 * @return returns a String that is the generated code
+	 */
+	public static String genSetterCode(String get, String set) {
+		Class getClass = null;
+		Class setClass = null;
+		//long start = new Date().getTime();
+		try {
+			getClass = ClassLoader.getSystemClassLoader().loadClass(get);
+			setClass = ClassLoader.getSystemClassLoader().loadClass(set);
+			List<Method> getClassMethods = JelperUtil.getClassMethods(getClass, GET);
+			List<Method> setClassInitMethods = JelperUtil.getClassMethods(setClass, SET);
+			List<Method> setClassMethods = new LinkedList<Method>();
+			// System.out.println(getClassMethods);
+			// System.out.println(setClassMethods);
+
+			List<String> setNameList= new ArrayList<String>();
+			List<Method> setList= new LinkedList<Method>();
+			for ( int i=0; i< setClassInitMethods.size();  i++){
+				setNameList.add(setClassInitMethods.get(i).getName());
+			}
+			Collections.sort(setNameList);
+			for( int i=0; i < setNameList.size(); i++){
+				for( int j=0 ;j < setClassInitMethods.size(); j++){
+					if( setNameList.get(i).equals(setClassInitMethods.get(j).getName())){
+						setClassMethods.add( setClassInitMethods.get(j));
+						setList.add(setClassInitMethods.get(j));
+						break;
+					}
+				}
+			}
+			if (getClassMethods.size() != 0 && setClassMethods.size() != 0)// perfect
+																			// match
+				matchGetterToSetter(getClassMethods, setClassMethods,
+						PERFECT_MATCH);
+			if (getClassMethods.size() != 0 && setClassMethods.size() != 0) {
+				matchGetterToSetter(getClassMethods, setClassMethods,
+						IGNORE_CASE_IND_WORDS);
+			}
+
+			if (getClassMethods.size() != 0 && setClassMethods.size() != 0) {
+				matchGetterToSetter(getClassMethods, setClassMethods,
+						TRY_MATCH_SHORTENED);
+			}
+			if (getClassMethods.size() != 0 && setClassMethods.size() != 0) {
+				matchGetterToSetter(getClassMethods, setClassMethods,
+						TRY_MATCH_SHORTENED_SHUFFLED);
+			}
+			if (getClassMethods.size() != 0 && setClassMethods.size() != 0) {
+				matchGetterToSetter(getClassMethods, setClassMethods,
+						MATCH_REMAINING_TYPE);
+			}
+
+			generateCode(setList);
+			//System.out.println("\nTook :" + (new Date().getTime() - start) + "ms");
+
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return gendCode.toString();
+	}
+
+	private static void generateCode(List<Method> setClassMethods) {
+		// Set<String> stringSet= codeMap.keySet();
+		for (int i = 0; i < setClassMethods.size(); i++) {
+			if (codeMap.containsKey(setClassMethods.get(i))) {
+				genSetCode(setClassMethods.get(i),
+						codeMap.get(setClassMethods.get(i)));
+			}
+		}
+	}
+
+
+	private static void matchGetterToSetter(List<Method> getClassMethods,
+			List<Method> setClassMethods, String matchLevel) {
+		String setName = null;
+		String getName = null;
+		String setType= null;
+		String getType= null;
+		if (PERFECT_MATCH.equalsIgnoreCase(matchLevel)) {
+			for (int i = 0; i < setClassMethods.size(); i++) {
+				setType = setClassMethods.get(i).getParameterTypes()[0].getSimpleName();
+				setName = setClassMethods.get(i).getName().substring(3);
+				for (int j = 0; j < getClassMethods.size(); j++) {
+					getType = getClassMethods.get(j).getReturnType().getSimpleName();
+					getName = getClassMethods.get(j).getName().substring(3);
+					if (setName.equals(getName)) {
+						codeMap.put(setClassMethods.get(i), getClassMethods.get(j));
+						//genSetCode(setClassMethods.get(i), getClassMethods.get(j));
+						setClassMethods.remove(i);
+						getClassMethods.remove(j);
+						i--;//to stop skipping the next element, since removing moves the elements 1 step up
+						break;
+					}
+//					else
+//						j++;
+				}
+			}
+
+		} else if (IGNORE_CASE_IND_WORDS.equalsIgnoreCase(matchLevel)) {
+
+			for (int i = 0; i < setClassMethods.size(); i++) {
+				setType = setClassMethods.get(i).getParameterTypes()[0].getSimpleName();
+				setName = setClassMethods.get(i).getName().substring(3);
+				for (int j = 0; j < getClassMethods.size(); j++) {
+					getName = getClassMethods.get(j).getName().substring(3);
+					if (setName.equalsIgnoreCase(getName)) {
+
+						codeMap.put(setClassMethods.get(i), getClassMethods.get(j));
+						//genSetCode(setClassMethods.get(i), getClassMethods.get(j));
+						setClassMethods.remove(i);
+						getClassMethods.remove(j);
+						i--;
+						break;
+					}
+//					else {					
+//						j++;
+//					}
+				}
+
+			}
+
+		}
+		else if(TRY_MATCH_SHORTENED.equals(matchLevel)){
+			boolean flag=false;
+			List<String> setMethWords=null;
+			List<String> getMethWords=null;
+			for (int i = 0; i < setClassMethods.size(); i++) {
+				setType = setClassMethods.get(i).getParameterTypes()[0].getSimpleName();
+				setName = setClassMethods.get(i).getName().substring(3);
+				for (int j = 0; j < getClassMethods.size(); j++) {
+					getName = getClassMethods.get(j).getName().substring(3);
+					setMethWords= JelperUtil.getWordsInMethodName(setName);
+					getMethWords= JelperUtil.getWordsInMethodName(getName);
+					if( JelperUtil.isShortenedMatching( setMethWords, getMethWords)){
+						codeMap.put(setClassMethods.get(i), getClassMethods.get(j));
+						//genSetCode(setClassMethods.get(i), getClassMethods.get(j));
+						setClassMethods.remove(i);
+						getClassMethods.remove(j);
+						i--;
+						break;
+					}
+//					else {
+//						// flag=false;
+//						j++;
+//					}
+				}
+			}
+		}
+		else if(TRY_MATCH_SHORTENED_SHUFFLED.equals(matchLevel)){
+			boolean flag=false;
+			List<String> setMethWords=null;
+			List<String> getMethWords=null;
+			List<String> getMethWordsLList= null;
+			for (int i = 0; i < setClassMethods.size(); i++) {
+				setName = setClassMethods.get(i).getName().substring(3);
+				for (int j = 0; j < getClassMethods.size(); j++) {
+					getName = getClassMethods.get(j).getName().substring(3);
+					setMethWords= JelperUtil.getWordsInMethodName(setName);
+					getMethWords= JelperUtil.getWordsInMethodName(getName);
+
+					for(int k=0; k< getMethWords.size(); k++){
+						flag= false;
+						getMethWordsLList =getMethWords;//necessary here
+						if( k< getMethWords.size()-1){
+							String prev= getMethWordsLList.get(k);
+							getMethWordsLList.set(k, getMethWordsLList.get(k+1));
+							getMethWordsLList.set(k+1, prev);
+						}
+
+						if( JelperUtil.isShortenedMatching( setMethWords, getMethWordsLList)){
+							codeMap.put(setClassMethods.get(i), getClassMethods.get(j));
+							//genSetCode(setClassMethods.get(i), getClassMethods.get(j));
+							setClassMethods.remove(i);
+							getClassMethods.remove(j);
+							i--;
+							flag= true;
+							break;
+						}
+					}
+
+					if(!flag){
+						j++;
+					}else{
+						break;
+					}
+				}
+			}
+		}
+		else if(MATCH_REMAINING_TYPE.equals(matchLevel)){
+			for( int i= 0 ; i<setClassMethods.size(); i++){
+				if( i < getClassMethods.size()){
+					if( setClassMethods.get(i).getParameterTypes()[0].equals(getClassMethods.get(i).getReturnType())){
+						codeMap.put(setClassMethods.get(i), getClassMethods.get(i));
+					}
+					//genSetCode(setClassMethods.get(i), getClassMethods.get(j));
+				}
+			}
+		}
+
+	}
+
+	private static void genSetCode(Method setMethod, Method getMethod) {
+		String getObjName = null;
+		String setObjName = null;
+		getObjName = JelperUtil.formatDeclaringClassNameAsCamel(getMethod.getDeclaringClass());
+		setObjName = JelperUtil.formatDeclaringClassNameAsCamel(setMethod.getDeclaringClass());
+		gendCode.append(setObjName);
+		gendCode.append(PERIOD);
+		gendCode.append(setMethod.getName());
+		gendCode.append(OPEN_PARAN);
+		gendCode.append(JelperUtil.isTypeCastNeeded(setMethod, getMethod));
+		//gendCode.append(PAIR_PARAN);
+		gendCode.append(CLOSED_PARAN);
+		gendCode.append(SEMI_COLON);
+		gendCode.append(NEW_LINE);
+	}
+
+	private static void genJdbcCode(Method setMethod, String column){
+		String setObjName = null;
+		String resMethod= JelperUtil.getResultSetGetterMethod(setMethod);
+		setObjName = JelperUtil.formatDeclaringClassNameAsCamel(setMethod.getDeclaringClass());
+		gendJdbcCode.append(setObjName);
+		gendJdbcCode.append(PERIOD);
+		gendJdbcCode.append(setMethod.getName());
+		gendJdbcCode.append(OPEN_PARAN);
+		gendJdbcCode.append("rs");
+		gendJdbcCode.append(PERIOD);
+		gendJdbcCode.append(resMethod);
+		gendJdbcCode.append(OPEN_PARAN);
+		gendJdbcCode.append(DOUBLE_QUOTE);
+		gendJdbcCode.append(column);
+		gendJdbcCode.append(DOUBLE_QUOTE);
+		gendJdbcCode.append(CLOSED_PARAN);
+		gendJdbcCode.append(CLOSED_PARAN);
+		gendJdbcCode.append(SEMI_COLON);
+		gendJdbcCode.append(NEW_LINE);
+	}
+}
---
+++ src/JelperUtil.java
@@ -0,0 +1,181 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+public class JelperUtil {
+
+	protected static List<Method> getClassMethods(Class classObj,String meth) {
+		List<Method> methods= new ArrayList<Method>();
+		Method[] methodArr=classObj.getDeclaredMethods();
+		 
+		for( int i=0 ;i<methodArr.length; i++){
+			if( meth.equalsIgnoreCase(methodArr[i].getName().substring(0,3))){
+				methods.add(methodArr[i]);
+			}
+		}
+		//methods= new ArrayList(Arrays.asList(classObj.getDeclaredMethods()));		
+		return methods;
+	}
+
+	protected static String formatDeclaringClassNameAsCamel(Class classOb) {
+		String str=classOb.getName();
+		str=str.replaceFirst(str.substring(0, 1),str.substring(0, 1).toLowerCase() );
+		return str;
+	}
+
+	protected static List<String> getWordsInMethodName(String name) {
+		List<String> words= new ArrayList<String>();
+		name=(name.substring(0,3).equals("set") || name.substring(0,3).equals("get"))?name.substring(3):name;//remove "set" or "get"
+		int j=0;
+		for( int i =1 ; i< name.length(); i++){
+			if( name.charAt(i) > 64 && name.charAt(i)< 91){
+				words.add(name.substring(j, i));
+				j=i;
+			}
+			if(i==name.length()-1){
+				words.add(name.substring(j));
+			}
+		}
+		return words;
+	}
+
+	protected static boolean isWordsMatching(Method setMethod,
+			Method getMethod) {
+				return false;/*
+		int count=-1;
+		setMethod.getName().
+		if(setMethodWords.size() == getMethodWords.size()){
+			
+			for( int i =0 ; i< setMethodWords.size(); i++){
+				if(setMethodWords.get(i).equalsIgnoreCase(getMethodWords.get(i))){
+					count++;
+				}
+			}
+		}
+		if(count == setMethodWords.size())
+			return true;
+		return false;
+	*/}
+
+	protected static String getResultSetGetterMethod(Method setMethod) {
+		StringBuilder str= new StringBuilder();
+		str.append("get");
+		String qualType=setMethod.getParameterTypes()[0].getName();
+		String type= capitalizeFirstLetter(qualType.substring(qualType.lastIndexOf(".")+1));
+		str.append(type);
+		return str.toString();
+	}
+
+	private static String capitalizeFirstLetter(String substring) {
+		String first=substring.substring(0,1);
+		substring= substring.replaceFirst(first, first.toUpperCase());
+		return substring;
+	}
+
+	protected static boolean isColumnMatching(String name, String column) {
+		if( name.equalsIgnoreCase(column.replaceAll("[_-]","")))
+			return true;
+		return false;
+		
+	}
+
+	protected static String genInstantiationCode(Class classOb) {
+			StringBuilder sb= new StringBuilder();
+			sb.append(classOb.getName());
+			sb.append(" ");
+			sb.append(formatDeclaringClassNameAsCamel(classOb));
+			sb.append(" = ");
+			sb.append("new");
+			sb.append(" ");
+			sb.append(classOb.getName());
+			sb.append("()");
+			sb.append(";");
+			sb.append("\n");
+			return sb.toString();
+		
+	}
+
+	protected static String getResSetInstCode() {
+		StringBuilder code= new StringBuilder();
+		code.append("Statement stmt = connection.createStatement();");
+		code.append("\n");
+		code.append("ResultSet rs = stmt.executeQuery(query);");
+		code.append("\n");
+		return code.toString();
+	}
+
+	protected static boolean isShortenedMatching(List<String> setMethWords,
+			List<String> getMethWords) {
+		String setWord, getWord;
+		int count=0;
+		if( setMethWords.size()==getMethWords.size()){
+			for( int i=0; i< setMethWords.size(); i++){
+				setWord= setMethWords.get(i);
+				getWord= getMethWords.get(i);
+				if( setWord.equalsIgnoreCase(getWord))
+					count++;
+				else{
+					if(!isWordMatchingShortened( setWord, getWord)){//if there is even one mismatch between words
+																	//there is no point in continuing , return false
+						return false;
+					}else{
+						count++;
+					}
+				}
+				if( count==setMethWords.size()){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	
+	private static boolean isWordMatchingShortened(String setWord, String getWord) {
+		String shortStr, longStr;
+		if( setWord.length()<getWord.length()){
+			shortStr=setWord;
+			longStr=getWord;
+		}else{
+			shortStr=getWord;
+			longStr=setWord;
+		}		
+		int j=0;
+		
+		if(shortStr.length()< 2 || longStr.charAt(0)!=shortStr.charAt(0))//if the first letter not ewual then false straight away
+			return false;
+		
+		for( int i=0 ; i< shortStr.length() && j< longStr.length(); i++, j++){// to see if the letters in shortStr are occuring in longStr in order
+			if(! longStr.substring(j,j+1).equals(shortStr.substring(i,i+1))){
+				--i;
+			}
+			if( i== shortStr.length()-1)//if all the letters are in order
+				return true;
+		}
+		return false;
+	}
+
+	protected static String isTypeCastNeeded(Method setMethod, Method getMethod) {
+		String getObjName=null;
+		getObjName = JelperUtil.formatDeclaringClassNameAsCamel(getMethod.getDeclaringClass());
+		StringBuilder code= new StringBuilder();
+		Class set= setMethod.getParameterTypes()[0];
+		if( !set.equals(getMethod.getReturnType()) &&
+				Jelper.compatTypes.containsKey(set)){
+			if( Jelper.compatTypes.get(set).equals(getMethod.getReturnType()))
+				code.append("new " + set.getSimpleName() + Jelper.OPEN_PARAN +Jelper.CLOSED_PARAN);
+				code.insert(code.toString().lastIndexOf(")"),getObjName + Jelper.PERIOD + getMethod.getName() + Jelper.PAIR_PARAN);
+				return code.toString();
+			}
+//			getObjName = Util.formatDeclaringClassNameAsCamel(getMethod.getDeclaringClass());
+//			return getObjName + Jelper.PERIOD + getMethod.getName();
+		else {
+			code.append(getObjName + Jelper.PERIOD + getMethod.getName()  + Jelper.PAIR_PARAN);
+			return code.toString();
+		}
+	}
+}
